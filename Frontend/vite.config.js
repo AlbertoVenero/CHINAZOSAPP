91069d9a5529f3a1206e4434d65bfdc4dcb52972
AlbertoVenero <38893617+AlbertoVenero@users.noreply.github.com>
2026-08-27 12:39:16 -0400

    Add preview configuration to Vite setup
    arreglos de preview

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -12,4 +12,9 @@ export default defineConfig({
     css: {
         postcss: './postcss.config.js',
     },
+    preview: {
+        host: true, // Expone a 0.0.0.0
+        port: parseInt(process.env.PORT) || 4173,
+        allowedHosts: true // Permite que acepte peticiones desde el dominio .up.railway.app
+  }
 });
